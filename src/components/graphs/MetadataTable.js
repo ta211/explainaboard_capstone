351673d22e4fedbc33679276a0c02f1968ee00ae
An Tang <antang06211@gmail.com>
2023-04-26 22:52:23 -0400

    MetadataTable: display metadata inner fields, bug fixed

--- src/components/graphs/MetadataTable.js
+++ src/components/graphs/MetadataTable.js
@@ -16,25 +16,34 @@ export default function MetadataTable({
             dataIndex: 'name',
             render: (text) => <a>{text}</a>,
         },
-        ...Object.keys(systemsData[0]).slice(1, -2).map(
+        ...Object.keys(systemsData[0]).slice(1, 4).map(
             name => {
                 return {
                     title: variableNameToDisplay(name),
                     dataIndex: name,
                 };
             }
+        ),
+        ...Object.keys(systemsData[0].metadata).map(
+            name => {
+                return {
+                    title: variableNameToDisplay(name),
+                    dataIndex: name,
+                }
+            }
         )
     ];
 
     const data = systemsData.map((system, index) => {
+        // This part is still hardcoded
         return {
             key: system.name,
             name: system.name,
             accuracy: system.accuracy,
             precision: system.precision,
             recall: system.recall,
-            batch_size: system.batch_size,
-            learning_rate: system.learning_rate,
+            batch_size: system.metadata.batch_size,
+            learning_rate: system.metadata.learning_rate,
         }
     })
 
